05f69855c656d1e6dc4990fc62aa8c7b2926790e
montse <mgoncar046@g.educaand.es>
2026-03-31 11:00:55 +0200

    Creado Cliente y Tipo cliente por Montse

---
+++ src/main/java/es/tienda/modelos/Cliente.java
@@ -0,0 +1,23 @@
+package es.tienda.modelos;
+
+public class Cliente extends Persona {
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    private TipoCliente tipoCliente;
+    public Cliente(String nombre, String apellidos, String dni) {
+        super(nombre, apellidos, dni);
+        setTipoCliente(TipoCliente.POTENCIAL);
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.printf("Nombre: %s , DNI:%s, TipoCliente:%s",
+                getNombre(),getDni(),getTipoCliente());
+    }
+}
